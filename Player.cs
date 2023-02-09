543af07066c06b92a27c1e2dcba0fa3634a34a04
TranMinhVu222 <tranminhvu_t64@hus.edu.vn>
2023-02-09 16:54:07 +0700

    DatVT update

--- Player.cs
+++ Player.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 public class Player
 {
+	void DatVT()
+	{
+		//ahihi
+	}
     //file o main
     // da duoc chinh sua khi tao branch Develop va checkout no
     // chinh sua o branch Change_Develop, nhiem vu la merge branch voi branch Develop
