a93ba56b637f19057fa5bc39c95979df6743ddfc
TranMinhVu222 <tranminhvu_t64@hus.edu.vn>
2023-02-09 15:31:15 +0700

    TODO: chinh sua o branch Change_Develop, nhiem vu la merge branch voi branch Develop

--- Player.cs
+++ Player.cs
@@ -3,4 +3,5 @@ public class Player
 {
     //file o main
     // da duoc chinh sua khi tao branch Develop va checkout no
+    // chinh sua o branch Change_Develop, nhiem vu la merge branch voi branch Develop
 }
