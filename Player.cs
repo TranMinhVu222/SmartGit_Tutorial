cf529af59c2796da223f169ef80ad298ff54c3dd
TranMinhVu222 <tranminhvu_t64@hus.edu.vn>
2023-02-09 16:57:52 +0700

    sua gi day

--- Player.cs
+++ Player.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 public class Player
 {
+	void VuTM()
+	{
+	}
     //file o main
     // da duoc chinh sua khi tao branch Develop va checkout no
     // chinh sua o branch Change_Develop, nhiem vu la merge branch voi branch Develop
